be7af9144b1987d718b65fe114642e18c00b6916
Wolfgang Leithold <wolfleithold@gmail.com>
2024-09-28 23:21:54 -0700

    added connections

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const { prompt } = require("inquirer");
 const logo = require("asciiart-logo");
 const db = require("./db");
+
+init();
+
+function init() {
+  const logoText = logo({ name: "Employee Manager" }).render();
+  console.log(logoText);
+  loadMainPrompts();
+}
