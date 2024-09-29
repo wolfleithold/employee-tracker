d90cbd01fef884c63e6d1e3a3852830e15b3e0e9
Wolfgang Leithold <wolfleithold@gmail.com>
2024-09-29 00:21:32 -0700

    added roles querys and department querys

--- database/index.js
+++ database/index.js
@@ -51,4 +51,58 @@ class DB {
       employeeId,
     ]);
   }
+
+  findAllRoles() {
+    return this.query(
+      "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+    );
+  }
+
+  createRole(role) {
+    const { title, salary, department_id } = role;
+    return this.query(
+      "INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)",
+      [title, salary, department_id]
+    );
+  }
+
+  removeRole(roleId) {
+    return this.query("DELETE FROM role WHERE id = $1", [roleId]);
+  }
+
+  findAllDepartments() {
+    return this.query("SELECT department.id, department.name FROM department;");
+  }
+
+  viewDepartmentBudgets() {
+    return this.query(
+      "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id GROUP BY department.id, department.name;"
+    );
+  }
+
+  createDepartment(department) {
+    return this.query("INSERT INTO department (name) VALUES ($1)", [
+      department.name,
+    ]);
+  }
+
+  removeDepartment(departmentId) {
+    return this.query("DELETE FROM department WHERE id = $1", [departmentId]);
+  }
+
+  findAllEmployeesByDepartment(departmentId) {
+    return this.query(
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department on role.department_id = department.id WHERE department.id = $1;",
+      [departmentId]
+    );
+  }
+
+  findAllEmployeesByManager(managerId) {
+    return this.query(
+      "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = $1;",
+      [managerId]
+    );
+  }
 }
+
+module.exports = new DB();
