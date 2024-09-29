4248d24987fbc2d8e8c32c20aa18bfd608cc6bfe
Wolfgang Leithold <wolfleithold@gmail.com>
2024-09-29 14:14:07 -0700

    added dependencies

--- database/index.js
+++ database/index.js
@@ -1,4 +1,4 @@
-const pool = require("./connection");
+const pool = require("./connections");
 
 class DB {
   constructor() {}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { prompt } = require("inquirer");
 const logo = require("asciiart-logo");
-const db = require("./db");
+const db = require("./database");
 
 init();
 
